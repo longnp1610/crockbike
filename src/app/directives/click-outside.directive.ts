799b241439c026595a8166e324d80e5d27d98bf0
Long Nguyen <longnp1610@gmail.com>
2024-01-03 15:40:09 +0700

    Added ClickOutsideDirective

---
+++ src/app/directives/click-outside.directive.ts
@@ -0,0 +1,49 @@
+import { DOCUMENT } from '@angular/common';
+import {
+  AfterViewInit,
+  Directive,
+  ElementRef,
+  EventEmitter,
+  Inject,
+  OnDestroy,
+  Output,
+} from '@angular/core';
+import { Subscription, filter, fromEvent } from 'rxjs';
+
+@Directive({
+  selector: '[appClickOutside]',
+  standalone: true,
+})
+export class ClickOutsideDirective implements AfterViewInit, OnDestroy {
+  @Output() clickOutside = new EventEmitter<void>();
+
+  documentClickSubscription: Subscription | undefined;
+
+  constructor(
+    private elementRef: ElementRef,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.documentClickSubscription = fromEvent(this.document, 'click')
+      .pipe(
+        filter((event: any) => {
+          return !this.isInside(event.target as HTMLElement);
+        })
+      )
+      .subscribe(() => {
+        this.clickOutside.emit();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.documentClickSubscription?.unsubscribe();
+  }
+
+  isInside(elementToCheck: HTMLElement): boolean {
+    return (
+      elementToCheck === this.elementRef.nativeElement ||
+      this.elementRef.nativeElement.contains(elementToCheck)
+    );
+  }
+}
