21929a5f584fa15f19ae7ffdd26609b56b72dc27
Long Nguyen <longnp1610@gmail.com>
2024-01-03 15:33:33 +0700

    Config Routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,29 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+  withInMemoryScrolling,
+  withRouterConfig,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(
+      routes,
+      // withDebugTracing(), // <-- Use for debug router
+      withEnabledBlockingInitialNavigation(), // <-- Only for SSR, wait for full response from server before render views
+      withRouterConfig({
+        onSameUrlNavigation: 'reload',
+      }),
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'top',
+      })
+      // withHashLocation()
+    ),
+    provideClientHydration(),
+  ],
 };
