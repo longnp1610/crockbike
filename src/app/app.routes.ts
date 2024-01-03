3445faeb543a0e6dd0126bdb0949562af961456f
Long Nguyen <longnp1610@gmail.com>
2024-01-03 15:32:09 +0700

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { ProductPageComponent } from './components/pages/product-page/product-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    title: 'CrockBike',
+  },
+  {
+    path: 'product',
+    component: ProductPageComponent,
+    title: 'Product',
+  },
+  { path: '**', redirectTo: '' },
+];
